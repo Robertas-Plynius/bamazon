82b26ac3752b68dd67fb29001b08c8ce691677a0
Robertas Plynius <robertasplynius@yahoo.com>
2018-05-07 16:57:59 -0500

    mysql table working

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
-create database bamazon;
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
 
 use bamazon;
 
@@ -8,16 +10,17 @@ create table products(
     department_name VARCHAR(50) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quality INT(10) NOT NULL,
-    PRIMARY KEY(item_id),
-)create database bamazon;
+    product_sales DECIMAL(10,2) DEFAULT 0,
+    PRIMARY KEY(item_id)
+);
 
-use bamazon;
 
-create table products(
-    item_id INT AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR(50) NOT NULL,
-    department_name VARCHAR(50) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock_quality INT(10) NOT NULL,
-    PRIMARY KEY(item_id),
-)
+CREATE TABLE departments (
+	department_id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(50),
+    over_head_costs INT DEFAULT 100,
+    PRIMARY KEY(department_id)
+);
+
+
+
--- seed.sql
+++ seed.sql
@@ -1,8 +1,26 @@
-INSERT INTO products (products_name, department_name, price, stock_quality)
-VALUES ('Fortnite', 'Video Games', 59.99, 360),
-('Halo', 'Video Games', 29.98, 34),
-('Sour Patch Kids', 'Food and Drink', 4.29, 100),
-('Jorts', 'Apparel', 3.56, 45),
-('Mesh Shirt', 'Apparel', 45.67, 8),
-('Lion King', 'Films', 19.99, 298),
-('Monopoly', 'Board Games', 30.50, 3429)
+INSERT INTO products (product_name, department_name, price, stock_quality)
+VALUES  ('Fortnite', 'Video Games', 59.99, 360),
+        ('Halo', 'Video Games', 29.98, 34),
+        ('Sour Patch Kids', 'Food', 4.29, 100),
+        ('Jorts', 'Apparel', 3.56, 45),
+        ('Mesh Shirt', 'Apparel', 45.67, 8),
+        ('Lion King', 'Films', 19.99, 298),
+        ('Monopoly', 'Board Games', 30.50, 3429),
+        ('PS4', 'Video Game Console', 300.00, 10),
+        ('Guppy', 'Fish', 5.00, 100),
+        ('Hot Cheetos', 'Food', 1.99, 100);
+
+INSERT INTO departments(department_name, over_head_costs)
+VALUES	('Video Games', 100), 
+		('Food', 150), 
+		('Apparel', 500), 
+        ('Films', 300),
+        ('Board Games', 100),
+        ('Video Game Console', 200),
+        ('Fish', 2);
+        
+        
+SELECT * FROM products
+
+
+
