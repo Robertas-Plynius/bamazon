df2ca9e1bfdb9290a28eac57b0a06e38b9512875
Robertas Plynius <robertasplynius@yahoo.com>
2018-05-07 16:00:37 -0500

    node modules added

---
+++ schema.sql
@@ -0,0 +1,23 @@
+create database bamazon;
+
+use bamazon;
+
+create table products(
+    item_id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quality INT(10) NOT NULL,
+    PRIMARY KEY(item_id),
+)create database bamazon;
+
+use bamazon;
+
+create table products(
+    item_id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quality INT(10) NOT NULL,
+    PRIMARY KEY(item_id),
+)
---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (products_name, department_name, price, stock_quality)
+VALUES ('Fortnite', 'Video Games', 59.99, 360),
+('Halo', 'Video Games', 29.98, 34),
+('Sour Patch Kids', 'Food and Drink', 4.29, 100),
+('Jorts', 'Apparel', 3.56, 45),
+('Mesh Shirt', 'Apparel', 45.67, 8),
+('Lion King', 'Films', 19.99, 298),
+('Monopoly', 'Board Games', 30.50, 3429)
